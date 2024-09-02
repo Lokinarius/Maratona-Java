5aba479f0f02526ca7f2f5f51fdc2e7d9787f298
William Galvão <Liam_draven@outlook.com>
2024-09-02 18:51:21 -0300

    exercício 1

---
+++ src/academy/devdojo/maratonajava/introducao/Aula003TiposPrimitivosExercicios.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula003TiposPrimitivosExercicios {
+   public static void main(String[] args){
+       String nome = "William";
+       String endereco = "Sobral - CE";
+       double salario = 5000.00;
+       String data = "20/08/2024";
+       String relatorio =
+               "Eu " + nome + ", morando na cidade de "+ endereco + ",confirmo que recebi o salário de " + salario + ",na data " + data;
+       // Saída de dados
+       System.out.println(relatorio);
+   }
+}
