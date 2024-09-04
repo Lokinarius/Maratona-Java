b2a62bd9db18882323d31ff039bb16c0bcdf161b
William Galvão <Liam_draven@outlook.com>
2024-09-04 16:47:54 -0300

    aprimoramentos Condicionais

--- src/academy/devdojo/maratonajava/introducao/Aula005EstruturasCondicionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula005EstruturasCondicionais01.java
@@ -1,16 +1,19 @@
 package academy.devdojo.maratonajava.introducao;
-
+import java.util.Scanner; // import scanner to read user input
 public class Aula005EstruturasCondicionais01 {
     public static void main(String[] args) {
         /* ESTRUTURAS CONDICIONAIS */
-        int age = 18;
+        Scanner scanner = new Scanner(System.in); // Create a Scanner object
+
+        System.out.print("Digite sua idade: ");
+        int age = scanner.nextInt();
         String idade;
 
         if (age <= 12) {
             idade = "Você é uma criança.";
-        } else if (age > 12 && age < 18) {
+        } else if (age < 18) {
             idade = "Você é adolescente.";
-        } else if (age >= 18 && age <= 60){
+        } else if (age <= 60){
             idade = "Você é adulto.";
         }else{
             idade = "Você é idoso.";
