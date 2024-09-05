8b1e89ecf06efa7f6862096c3ef96d042021c12d
William Galvão <Liam_draven@outlook.com>
2024-09-05 18:54:40 -0300

    update

--- src/academy/devdojo/maratonajava/introducao/Aula005EstruturasCondicionais02OperadorTernario.java
+++ src/academy/devdojo/maratonajava/introducao/Aula005EstruturasCondicionais02OperadorTernario.java
@@ -5,7 +5,10 @@ public class Aula005EstruturasCondicionais02OperadorTernario {
         /* OPERADOR TERNÁRIO */
         // (Condição)? Valor caso verdadeiro : Valor caso falso
         int age = 18;
-        String message = (age <= 12)? "Você é uma criança." : (age > 12 && age < 18)? "Você é adolescente." : "Você é adulto.";
+        String message = (age <= 12) ? "Você é uma criança." :
+                         (age < 18) ? "Você é adolescente." :
+                         (age < 60) ? "Você é adulto." :
+                                    "Você é idoso";
         System.out.println(message);
     }
 }
