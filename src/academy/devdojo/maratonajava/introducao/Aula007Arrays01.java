63d42ddd6ee9744068af556bb1c7fdccb33e55ec
William Galvão <Liam_draven@outlook.com>
2024-09-15 21:04:18 -0300

    Update Aula007Arrays01.java

--- src/academy/devdojo/maratonajava/introducao/Aula007Arrays01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula007Arrays01.java
@@ -19,5 +19,13 @@ public class Aula007Arrays01 {
         // Remover um elemento do array
         numeros[5] = 0; // Limpando o valor do último elemento
         System.out.println("Novo valor do último elemento: " + numeros[5] );
+
+        int [] idades = new int[3];
+        idades[0] = 29;
+        idades[1] = 32;
+        idades[2] = 27;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
     }
 }
