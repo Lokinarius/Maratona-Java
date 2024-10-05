a99c280fadafd5442c765201dc503e9b5a855ce0
William Galvão <Liam_draven@outlook.com>
2024-10-05 15:50:25 -0300

    Create Calculadora.java

---
+++ src/academy/devdojo/maratonajava/javacore/introducao02metodos/dominio/Calculadora.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.introducao02metodos.dominio;
+
+public class Calculadora {
+    //[SOMA]
+    public int somar(int a, int b) {
+        return a + b;
+    }
+    //[SUBTRAÇÃO]
+    public double subtrair(double a, double b) {
+        return a - b;
+    }
+    //[MULTIPLICAÇÃO]
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+    //[DIVISÃO]
+    public double dividir(double a, double b) {
+        return a / b;
+    }
+}
