11d7fd7d7cfca9e9aa3531be551a7de57069bb32
William Galvão <Liam_draven@outlook.com>
2024-10-08 19:01:24 -0300

    Create Calculadoratest02.java

---
+++ src/academy/devdojo/maratonajava/javacore/introducao02metodos/teste/Calculadoratest02.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.introducao02metodos.teste;
+//imports
+import academy.devdojo.maratonajava.javacore.introducao02metodos.dominio.Calculadora;
+import java.util.Scanner;
+public class Calculadoratest02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Create a Scanner object
+        Calculadora cal = new Calculadora();
+
+    }
+}
