11005959f4c83a19ef5ab9e5b8b06c7cd6d2105d
William Galvão <Liam_draven@outlook.com>
2024-10-17 19:35:04 -0300

    Create EstudanteTeste01.java

---
+++ src/academy/devdojo/maratonajava/javacore/introducao02metodos/teste/EstudanteTeste01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.introducao02metodos.teste;
+
+import academy.devdojo.maratonajava.javacore.introducao02metodos.dominio.Estudante;
+
+public class EstudanteTeste01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+    }
+}
