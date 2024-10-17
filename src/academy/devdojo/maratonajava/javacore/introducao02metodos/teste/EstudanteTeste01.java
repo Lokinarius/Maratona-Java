343e37077d361f21169b4c80ef2512ee39d426de
William Galvão <Liam_draven@outlook.com>
2024-10-17 19:42:36 -0300

    Update EstudanteTeste01.java

--- src/academy/devdojo/maratonajava/javacore/introducao02metodos/teste/EstudanteTeste01.java
+++ src/academy/devdojo/maratonajava/javacore/introducao02metodos/teste/EstudanteTeste01.java
@@ -1,9 +1,23 @@
 package academy.devdojo.maratonajava.javacore.introducao02metodos.teste;
 
 import academy.devdojo.maratonajava.javacore.introducao02metodos.dominio.Estudante;
+import academy.devdojo.maratonajava.javacore.introducao02metodos.dominio.ImpressoraEstudante;
 
 public class EstudanteTeste01 {
     public static void main(String[] args) {
-        Estudante estudante = new Estudante();
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
+
+        estudante01.nome = "Naruto";
+        estudante01.idade = 15;
+        estudante01.sexo = 'M';
+
+        estudante02.nome = "Sakura";
+        estudante02.idade = 15;
+        estudante02.sexo = 'F';
+
+        impressora.imprime(estudante01);
+        impressora.imprime(estudante02);
     }
 }
