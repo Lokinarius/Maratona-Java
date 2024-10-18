43c83d4e8c8e1aae1a8af97c26e5811653600396
William Galvão <Liam_draven@outlook.com>
2024-10-18 16:51:27 -0300

    Update ImpressoraEstudante.java

--- src/academy/devdojo/maratonajava/javacore/introducao02metodos/dominio/ImpressoraEstudante.java
+++ src/academy/devdojo/maratonajava/javacore/introducao02metodos/dominio/ImpressoraEstudante.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.javacore.introducao02metodos.dominio;
 
 public class ImpressoraEstudante {
     public void imprime(Estudante estudante) {
+        System.out.println("--------------------------------");
         System.out.println("Nome: " + estudante.nome);
         System.out.println("Idade: " + estudante.idade);
         System.out.println("Sexo: " + estudante.sexo);
