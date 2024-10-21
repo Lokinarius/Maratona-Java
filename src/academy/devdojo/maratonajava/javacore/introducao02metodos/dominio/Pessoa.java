d3841aff8c5bea641a17ed592843d44479fdee96
William Galvão <Liam_draven@outlook.com>
2024-10-21 18:07:19 -0300

    Create Pessoa.java

---
+++ src/academy/devdojo/maratonajava/javacore/introducao02metodos/dominio/Pessoa.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.introducao02metodos.dominio;
+
+public class Pessoa {
+    private String nome;
+    public int idade;
+    private char sexo;
+
+    public void imprime(){
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        System.out.println("Sexo: " + this.sexo);
+    }
+
+    // [GETTERS AND SETTERS]
+    // Nome
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    // Sexo
+    public char getSexo() {
+        return sexo;
+    }
+    public void setSexo(char sexo) {
+        if(sexo != 'm' && sexo != 'f'){
+            throw new IllegalArgumentException("Sexo deve ser'm' ou 'f'");
+        }
+        this.sexo = sexo;
+    }
+}
