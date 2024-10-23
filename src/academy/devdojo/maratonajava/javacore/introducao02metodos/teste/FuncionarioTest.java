a61f9ff68ebc69a0fc3702a188468fe67d88a84a
William Galvão <Liam_draven@outlook.com>
2024-10-23 18:15:36 -0300

    Update FuncionarioTest.java

--- src/academy/devdojo/maratonajava/javacore/introducao02metodos/teste/FuncionarioTest.java
+++ src/academy/devdojo/maratonajava/javacore/introducao02metodos/teste/FuncionarioTest.java
@@ -4,29 +4,24 @@ import academy.devdojo.maratonajava.javacore.introducao02metodos.dominio.Funcion
 
 public class FuncionarioTest {
     public static void main(String[] args) {
+        //Criação e inicialização dos objetos de funcionário
         Funcionario funcionario01 = new Funcionario();
         Funcionario funcionario02 = new Funcionario();
         Funcionario funcionario03 = new Funcionario();
 
-        funcionario01.nome = "Rei";
-        funcionario01.idade = 17;
-        funcionario01.salario[0] = 1500.0;
-        funcionario01.salario[1] = 1800.0;
-        funcionario01.salario[2] = 2000.0;
+        funcionario01.setNome("Rei");
+        funcionario01.setIdade(17);
+        funcionario01.setSalario(new double[]{1600.0,1700.0,2000,0});
 
-        funcionario02.nome = "Asuka";
-        funcionario02.idade = 18;
-        funcionario02.salario[0] = 1700.0;
-        funcionario02.salario[1] = 1900.0;
-        funcionario02.salario[2] = 2100.0;
+        funcionario02.setNome("Asuka");
+        funcionario02.setIdade(17);
+        funcionario02.setSalario(new double[]{1500.0,1800.0,2100.0});
 
-        funcionario03.nome = "Shinji";
-        funcionario03.idade = 19;
-        funcionario03.salario[0] = 1600.0;
-        funcionario03.salario[1] = 1850.0;
-        funcionario03.salario[2] = 2050.0;
+        funcionario03.setNome("Shinji");
+        funcionario03.setIdade(17);
+        funcionario03.setSalario(new double[]{1800.0,1900.0,2200.0});
 
-        //Saída
+        // Saída
         funcionario01.mediaSalarial();
         funcionario01.imprime();
 
