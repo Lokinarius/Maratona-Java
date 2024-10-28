7b29d57b0617dc4284ee18b277aac0babab56174
William Galvão <Liam_draven@outlook.com>
2024-10-28 16:05:51 -0300

    Update Animetest.java

--- src/academy/devdojo/maratonajava/javacore/introducao04construtores/teste/Animetest.java
+++ src/academy/devdojo/maratonajava/javacore/introducao04construtores/teste/Animetest.java
@@ -1,4 +1,11 @@
 package academy.devdojo.maratonajava.javacore.introducao04construtores.teste;
 
+import academy.devdojo.maratonajava.javacore.introducao04construtores.domain.Anime;
+
 public class Animetest {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Soul Eater");
+        anime.init("Soul Eater","TV",48, "Shounen");
+        anime.imprime();
+    }
 }
