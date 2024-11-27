f9f343851abe45ce7f6def7b274e0742c2e63952
William Galvão <Liam_draven@outlook.com>
2024-11-27 16:27:45 -0300

    Create Carro.java

---
+++ src/academy/devdojo/maratonajava/javacore/introducao06modificadorestatico/domain/Carro.java
@@ -0,0 +1,48 @@
+package academy.devdojo.maratonajava.javacore.introducao06modificadorestatico.domain;
+
+public class Carro {
+    private String nome;
+    private double velocidadeMaxima;
+    private static double velocidadeLimite = 250;
+
+    // [CONSTRUTOR]
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    // [METODO IMPRIME]
+    public void imprime(){
+        System.out.println("===================");
+        System.out.println("Nome: "+this.nome);
+        System.out.println("Velocidade Máxima: "+this.velocidadeMaxima);
+        System.out.println("Velocidade Limite: "+this.velocidadeLimite);
+    }
+
+    // [GETTERS AND SETTERS]
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public double getVelocidadeLimite() {
+        return velocidadeLimite;
+    }
+
+    public void setVelocidadeLimite(double velocidadeLimite) {
+        this.velocidadeLimite = velocidadeLimite;
+    }
+}
+
+
