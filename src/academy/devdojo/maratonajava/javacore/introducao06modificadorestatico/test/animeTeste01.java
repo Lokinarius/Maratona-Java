83f73aae12093dddb73a5acabbbed87b7f430d07
William Galvão <Liam_draven@outlook.com>
2024-11-28 17:55:27 -0300

    Create animeTeste01.java

---
+++ src/academy/devdojo/maratonajava/javacore/introducao06modificadorestatico/test/animeTeste01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.introducao06modificadorestatico.test;
+
+import academy.devdojo.maratonajava.javacore.introducao06modificadorestatico.domain.Anime;
+
+public class animeTeste01 {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+
+    }
+}
