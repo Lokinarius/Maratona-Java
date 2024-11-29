2c3e696147885d5a33e7b084d4646a0b43749d2f
William Galvão <Liam_draven@outlook.com>
2024-11-29 14:42:22 -0300

    Create Time.java

---
+++ src/academy/devdojo/maratonajava/javacore/introducao07associacao/domain/Time.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.introducao07associacao.domain;
+
+public class Time {
+    private String nome;
+
+    public Time(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
