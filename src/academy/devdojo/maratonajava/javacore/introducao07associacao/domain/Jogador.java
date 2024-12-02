7c61144fff9782d3c2034d33b89d353b7c185277
William Galvão <Liam_draven@outlook.com>
2024-12-02 16:03:17 -0300

    Update Jogador.java

--- src/academy/devdojo/maratonajava/javacore/introducao07associacao/domain/Jogador.java
+++ src/academy/devdojo/maratonajava/javacore/introducao07associacao/domain/Jogador.java
@@ -2,20 +2,14 @@ package academy.devdojo.maratonajava.javacore.introducao07associacao.domain;
 
 public class Jogador {
     private String nome;
-
-    public Time getTime() {
-        return time;
-    }
-
-    public void setTime(Time time) {
-        this.time = time;
-    }
-
     private Time time;
 
     // Metodo que imprime o nome do jogador
     public void imprime(){
         System.out.println("Nome: " + this.nome);
+        if(time != null){
+            System.out.println(time.getNome());
+        }
     }
 
     // Construtor
@@ -26,8 +20,13 @@ public class Jogador {
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
+    }
+    public Time getTime() {
+        return time;
+    }
+    public void setTime(Time time) {
+        this.time = time;
     }
 }
