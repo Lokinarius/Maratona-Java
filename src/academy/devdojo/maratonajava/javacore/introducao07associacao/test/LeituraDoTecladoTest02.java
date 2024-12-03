69ff7a3a99236e15130f84db6b420f0fb556b1b1
William Galvão <Liam_draven@outlook.com>
2024-12-03 14:43:44 -0300

    Create LeituraDoTecladoTest02.java

---
+++ src/academy/devdojo/maratonajava/javacore/introducao07associacao/test/LeituraDoTecladoTest02.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.introducao07associacao.test;
+
+import java.util.Scanner;
+
+public class LeituraDoTecladoTest02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Previsão do futuro");
+        System.out.println("Digite sua pergunta e responderei Sim ou não");
+        String pergunta = input.nextLine();
+        System.out.println("Você perguntou: " + pergunta);
+        if(pergunta.charAt(0) == ' '){
+            System.out.println("SIM");
+        }else{
+            System.out.println("NÃO");
+        }
+    }
+}
