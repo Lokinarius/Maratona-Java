0d9016d5ec76c68fe52ab6eb80e145ccfc6bc9f5
William Galvão <Liam_draven@outlook.com>
2024-12-03 14:43:34 -0300

    update

--- src/academy/devdojo/maratonajava/javacore/introducao07associacao/domain/Professor.java
+++ src/academy/devdojo/maratonajava/javacore/introducao07associacao/domain/Professor.java
@@ -2,16 +2,31 @@ package academy.devdojo.maratonajava.javacore.introducao07associacao.domain;
 
 public class Professor {
     private String nome;
+    private String materia;
+    private Seminario[] seminarios;
 
+    // Construtor
+    public Professor(String nome) {
+        this.nome = nome;
+    }
+     // Construtor sobrecarregado
+    public Professor(String nome, String materia, Seminario[] seminarios) {
+        this.nome = nome;
+        this.materia = materia;
+        this.seminarios = seminarios;
+    }
+
+    // Getters and setters
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
-    public Professor(String nome) {
-        this.nome = nome;
+    public String getMateria() {
+        return materia;
+    }
+    public void setMateria(String materia) {
+        this.materia = materia;
     }
 }
