c9d7cb67cdd216ef0324d1ef6ae86037cfc4901b
William Galvão <Liam_draven@outlook.com>
2024-12-04 12:49:32 -0300

    Update Professor.java

--- src/academy/devdojo/maratonajava/javacore/introducao07associacao/domain/Professor.java
+++ src/academy/devdojo/maratonajava/javacore/introducao07associacao/domain/Professor.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.introducao07associacao.domain;
 
 public class Professor {
+    // Atributos
     private String nome;
     private String materia;
     private Seminario[] seminarios;
@@ -9,11 +10,38 @@ public class Professor {
     public Professor(String nome) {
         this.nome = nome;
     }
-     // Construtor sobrecarregado
+    public String materia(){
+        return materia;
+    }
+
+     // Construtores sobrecarregados
+     public Professor(String nome, String materia) {
+         this.nome = nome;
+         this.materia = materia;
+     }
     public Professor(String nome, String materia, Seminario[] seminarios) {
         this.nome = nome;
         this.materia = materia;
         this.seminarios = seminarios;
+     }
+
+    // Metodo imprime
+    public void imprime(){
+        System.out.println("---------------");
+        System.out.println("Professor: "+this.nome);
+        if(this.seminarios == null) return;
+        System.out.println("## Seminários cadastrados ##");
+        for (Seminario seminario : this.seminarios) {
+            System.out.println(seminario.getTitulo());
+            System.out.println(seminario.getLocal().getEndereco());
+            System.out.println("## Alunos ##");
+            for (Aluno aluno : seminario.getAlunos()) {
+                System.out.println("Aluno: "+aluno.getNome());
+                System.out.println("Idade: "+aluno.getIdade());
+            }
+
+        }
+
     }
 
     // Getters and setters
@@ -28,5 +56,8 @@ public class Professor {
     }
     public void setMateria(String materia) {
         this.materia = materia;
+    }
+
+    public void setSeminarios(Seminario[] seminariosDisponiveis) {
     }
 }
