8ec369b2746b9ae7dcf581f0817096c8c5b1a0b1
William Galvão <Liam_draven@outlook.com>
2024-12-04 12:49:36 -0300

    Update Local.java

--- src/academy/devdojo/maratonajava/javacore/introducao07associacao/domain/Local.java
+++ src/academy/devdojo/maratonajava/javacore/introducao07associacao/domain/Local.java
@@ -3,5 +3,16 @@ package academy.devdojo.maratonajava.javacore.introducao07associacao.domain;
 public class Local {
     private String endereco;
 
+    // Construtor
+    public Local(String endereco) {
+        this.endereco = endereco;
+    }
 
+    // Getters e Setters
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
