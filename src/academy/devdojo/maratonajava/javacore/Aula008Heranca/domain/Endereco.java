ed67345a96c864bdba1b2f809abf2e5e14d19a96
William Galvão <Liam_draven@outlook.com>
2024-12-09 17:00:07 -0300

    Create Endereco.java

---
+++ src/academy/devdojo/maratonajava/javacore/Aula008Heranca/domain/Endereco.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Aula008Heranca.domain;
+
+public class Endereco {
+    // Atributos
+    private String rua;
+    private String cep;
+
+    // Construtor
+
+
+    // Getters e Setters
+    public String getRua() {return rua;}
+    public void setRua(String rua) {this.rua = rua;}
+
+    public String getCep() {return cep;}
+    public void setCep(String cep) {this.cep = cep;}
+}
