884fcb0630b73123024e78bf4b8c73ea1b4054df
William Galvão <Liam_draven@outlook.com>
2024-12-09 17:00:10 -0300

    Create Funcionario.java

---
+++ src/academy/devdojo/maratonajava/javacore/Aula008Heranca/domain/Funcionario.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Aula008Heranca.domain;
+
+public class Funcionario extends Pessoa{
+    // atributos
+    private double salario;
+
+    // imprime
+    
+
+    // Gettters e Setters
+    public double getSalario() {return salario;}
+    public void setSalario(double salario) {this.salario = salario;}
+}
