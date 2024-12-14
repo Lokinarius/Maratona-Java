a5ef143913679d3909f0a25b434a3ec0d73cbb72
William Galvão <Liam_draven@outlook.com>
2024-12-14 17:17:25 -0300

    Create Animetest01.java

---
+++ src/academy/devdojo/maratonajava/javacore/Aula009Sobreescrita/test/Animetest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Aula009Sobreescrita.test;
+
+import academy.devdojo.maratonajava.javacore.Aula009Sobreescrita.domain.Anime;
+
+public class Animetest01 {
+    public static void main(String[] args) {
+        //[OBJETOS]
+        Anime anime = new Anime("Bleach");
+
+        //[IMPRESSÃO]
+        System.out.println(anime);
+    }
+}
