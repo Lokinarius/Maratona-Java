6246598d9c2018aebf9a71d996b46dd2e03bcad9
William Galvão <Liam_draven@outlook.com>
2024-12-16 17:42:47 -0300

    Update Carro.java

--- src/academy/devdojo/maratonajava/javacore/Aula010ModificadorFinal/domain/Carro.java
+++ src/academy/devdojo/maratonajava/javacore/Aula010ModificadorFinal/domain/Carro.java
@@ -1,12 +1,16 @@
 package academy.devdojo.maratonajava.javacore.Aula010ModificadorFinal.domain;
 
 public class Carro {
+    //[ATRIBUTOS]
     private String nome;
     public static final double VELOCIDADE_LIMITE = 250;
     // Constantes não podem ter o valor alterado
     // Constantes geralmente são acompanhadas do metod estatico
     // Constantes são descritas em Uppercase e separadas por underline caso tenham mais de uma palavra
+    public final Comprador COMPRADOR = new Comprador();
 
+
+    //[GETTER E SETTERS]
     public String getNome() {return nome;}
     public void setNome(String nome) {this.nome = nome;}
 }
