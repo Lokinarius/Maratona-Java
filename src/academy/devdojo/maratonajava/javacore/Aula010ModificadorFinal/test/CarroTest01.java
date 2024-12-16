979f4a5aba2e653a5239eae2ba7d257ccae17d3c
William Galvão <Liam_draven@outlook.com>
2024-12-16 17:42:45 -0300

    Update CarroTest01.java

--- src/academy/devdojo/maratonajava/javacore/Aula010ModificadorFinal/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aula010ModificadorFinal/test/CarroTest01.java
@@ -1,11 +1,15 @@
 package academy.devdojo.maratonajava.javacore.Aula010ModificadorFinal.test;
 
 import academy.devdojo.maratonajava.javacore.Aula010ModificadorFinal.domain.Carro;
-
+import academy.devdojo.maratonajava.javacore.Aula010ModificadorFinal.domain.Comprador;
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
 
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        Carro.COMPRADOR.setNome("Kuririn");
+        System.out.println(carro.COMPRADOR);
+
     }
 }
