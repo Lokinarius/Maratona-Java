e8ca49df35a9ee38214e5f70bc9ba6c36a54537f
William Galvão <Liam_draven@outlook.com>
2024-12-17 18:49:49 -0300

    Create Cliente.java

---
+++ src/academy/devdojo/maratonajava/javacore/Aula011Enumeracao/domain/Cliente.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Aula011Enumeracao.domain;
+
+public class Cliente {
+    // Atributos
+    private String nome;
+    private TipoCliente tipoCliente;
+
+    //To string
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", tipoCliente=" + tipoCliente +
+                '}';
+    }
+
+    //Construtor
+    public Cliente(String nome, TipoCliente tipoCliente) {
+        this.nome = nome;
+        this.tipoCliente = tipoCliente;
+    }
+
+    //Getters e Setters
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public TipoCliente getTipoCliente() {return tipoCliente;}
+    public void setTipoCliente(TipoCliente tipoCliente) {this.tipoCliente = tipoCliente;}
+}
