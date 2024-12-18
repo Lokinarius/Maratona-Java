6bc70c5ea3a52398c64ab7b05398fc6e7ffde2d2
William Galvão <Liam_draven@outlook.com>
2024-12-18 16:42:26 -0300

    Update ClientTest.java

--- src/academy/devdojo/maratonajava/javacore/Aula011Enumeracao/test/ClientTest.java
+++ src/academy/devdojo/maratonajava/javacore/Aula011Enumeracao/test/ClientTest.java
@@ -3,13 +3,16 @@ package academy.devdojo.maratonajava.javacore.Aula011Enumeracao.test;
 import academy.devdojo.maratonajava.javacore.Aula011Enumeracao.domain.Cliente;
 import academy.devdojo.maratonajava.javacore.Aula011Enumeracao.domain.TipoCliente;
 
+import static academy.devdojo.maratonajava.javacore.Aula011Enumeracao.domain.Cliente.TipoPagamento.CREDITO;
+import static academy.devdojo.maratonajava.javacore.Aula011Enumeracao.domain.Cliente.TipoPagamento.DEBITO;
+
 public class ClientTest {
     public static void main(String[] args) {
         // Objetos
-        Cliente cliente1 = new Cliente("Makima", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Power Rangers",TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente3 = new Cliente("Power",TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("time 7",TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Makima", TipoCliente.PESSOA_FISICA, DEBITO);
+        Cliente cliente2 = new Cliente("Power Rangers",TipoCliente.PESSOA_JURIDICA,DEBITO);
+        Cliente cliente3 = new Cliente("Power",TipoCliente.PESSOA_FISICA,CREDITO);
+        Cliente cliente4 = new Cliente("time 7",TipoCliente.PESSOA_JURIDICA, CREDITO);
 
         // Saída de dados
         System.out.println(cliente1);
