2647baf3221e7731119d792e5e180ca22c0709af
William Galvão <Liam_draven@outlook.com>
2024-12-18 16:35:30 -0300

    Update TipoCliente.java

--- src/academy/devdojo/maratonajava/javacore/Aula011Enumeracao/domain/TipoCliente.java
+++ src/academy/devdojo/maratonajava/javacore/Aula011Enumeracao/domain/TipoCliente.java
@@ -1,6 +1,16 @@
 package academy.devdojo.maratonajava.javacore.Aula011Enumeracao.domain;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1),
+    PESSOA_JURIDICA(2);
+
+    private final int VALOR;
+
+    TipoCliente(int valor) {
+        this.VALOR = valor;
+    }
+
+    public int getVALOR() {
+        return VALOR;
+    }
 }
