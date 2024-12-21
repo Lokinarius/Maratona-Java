7b03e7ad7cdcdd32780ddfab653e8c79ca58c5d0
William Galvão <Liam_draven@outlook.com>
2024-12-21 14:31:32 -0300

    Create Funcionario.java

---
+++ src/academy/devdojo/maratonajava/javacore/Aula012ClassesAbstratas/domain/Funcionario.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Aula012ClassesAbstratas.domain;
+
+public abstract class Funcionario {
+    // [ATRIBUTOS]
+    protected String nome;
+    protected double salario;
+
+    // [CONSTRUTOR]
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    // [TO STRING]
+
+
+    // [GETTERS E SETTERS]
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public double getSalario() {return salario;}
+    public void setSalario(double salario) {this.salario = salario;}
+}
