620ca1df4ea44991f8a9b7568a44ffb6386f1ddc
William Galvão <Liam_draven@outlook.com>
2025-01-01 15:02:47 -0300

    Update Tomate.java

--- src/academy/devdojo/maratonajava/javacore/Aula014Polimorfismo/domain/Tomate.java
+++ src/academy/devdojo/maratonajava/javacore/Aula014Polimorfismo/domain/Tomate.java
@@ -4,6 +4,9 @@ public class Tomate extends Produto{
     //[CONSTANTE]
     public static final double IMPOSTO = 0.27;
 
+    //[ATRIBUTOS]
+    private String dataValidade;
+
     //[CONSTRUTOR]
     public Tomate(String name, double valor) {
         super(name, valor);
@@ -14,4 +17,8 @@ public class Tomate extends Produto{
     public double calcularImposto() {
         return this.valor = IMPOSTO;
     }
+
+    //[GETTERS E SETTERS]
+    public String getDataValidade() {return dataValidade;}
+    public void setDataValidade(String dataValidade) {this.dataValidade = dataValidade;}
 }
