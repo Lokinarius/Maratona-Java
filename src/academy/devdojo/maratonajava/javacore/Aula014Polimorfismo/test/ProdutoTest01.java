03114771ed3401c5f8304d73ce394034921e5205
William Galvão <Liam_draven@outlook.com>
2025-01-02 15:39:18 -0300

    Update ProdutoTest01.java

--- src/academy/devdojo/maratonajava/javacore/Aula014Polimorfismo/test/ProdutoTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aula014Polimorfismo/test/ProdutoTest01.java
@@ -1,6 +1,8 @@
 package academy.devdojo.maratonajava.javacore.Aula014Polimorfismo.test;
 
 import academy.devdojo.maratonajava.javacore.Aula014Polimorfismo.domain.Computador;
+import academy.devdojo.maratonajava.javacore.Aula014Polimorfismo.domain.Produto;
+import academy.devdojo.maratonajava.javacore.Aula014Polimorfismo.domain.Televisao;
 import academy.devdojo.maratonajava.javacore.Aula014Polimorfismo.domain.Tomate;
 import academy.devdojo.maratonajava.javacore.Aula014Polimorfismo.service.CalculadoraImposto;
 
@@ -8,8 +10,12 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("NUC1017",7000);
         Tomate tomate = new Tomate("Tomate cereja",12);
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        Televisao tv = new Televisao("LG TV",1200);
+
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("---------------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("---------------------");
+        CalculadoraImposto.calcularImposto(tv);
     }
 }
