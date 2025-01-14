3fcc02bb72417817cdb01b6b12e7dad8ead76839
William Galvão <Liam_draven@outlook.com>
2025-01-14 16:34:38 -0300

    Create RuntimeExceptionTest01.java

---
+++ src/academy/devdojo/maratonajava/javacore/Aula015Exception/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Aula015Exception.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        // Checked e unchecked
+        int[] num = {1,2};
+        System.out.println(num[2]);
+    }
+}
