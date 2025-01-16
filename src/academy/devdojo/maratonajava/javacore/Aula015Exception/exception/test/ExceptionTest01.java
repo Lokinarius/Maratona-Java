c9edc0e1af68fb262f1bb4dfb256d6ad153970fa
William Galvão <Liam_draven@outlook.com>
2025-01-16 18:13:25 -0300

    Update ExceptionTest01.java

--- src/academy/devdojo/maratonajava/javacore/Aula015Exception/exception/test/ExceptionTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aula015Exception/exception/test/ExceptionTest01.java
@@ -4,17 +4,18 @@ import java.io.File;
 import java.io.IOException;
 
 public class ExceptionTest01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         criarNovoArquivo();
     }
 
-    public static void criarNovoArquivo(){
+    public static void criarNovoArquivo() throws IOException{
         File file = new File("arquivo\\teste.txt");
         try{
             boolean criado = file.createNewFile();
             System.out.println("Arquivo criado"+criado);
         }catch (IOException e){
             e.printStackTrace();
+            throw e;
         }
     }
 }
