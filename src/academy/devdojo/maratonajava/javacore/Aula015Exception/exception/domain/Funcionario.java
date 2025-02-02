3284fb0f0fc5f5e62e8d4bb3e94653bd8ae6f490
William Galvão <Liam_draven@outlook.com>
2025-02-02 05:21:45 -0300

    Create Funcionario.java

---
+++ src/academy/devdojo/maratonajava/javacore/Aula015Exception/exception/domain/Funcionario.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Aula015Exception.exception.domain;
+
+import java.io.FileNotFoundException;
+
+public class Funcionario extends Pessoa{
+    public void salvar() {
+        System.out.println("Salvando Funcionario");
+    }
+}
