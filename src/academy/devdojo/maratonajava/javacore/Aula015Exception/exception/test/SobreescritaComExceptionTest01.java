bb9c52cb14f55e61926e93b4f0749404034a2a85
William Galvão <Liam_draven@outlook.com>
2025-02-02 05:21:50 -0300

    Create SobreescritaComExceptionTest01.java

---
+++ src/academy/devdojo/maratonajava/javacore/Aula015Exception/exception/test/SobreescritaComExceptionTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Aula015Exception.exception.test;
+
+import academy.devdojo.maratonajava.javacore.Aula015Exception.exception.domain.Funcionario;
+import academy.devdojo.maratonajava.javacore.Aula015Exception.exception.domain.Pessoa;
+
+public class SobreescritaComExceptionTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        Funcionario funcionario = new Funcionario();
+
+
+    }
+}
