889757bfcc2328e356dd0515a1fee8f984c4f08e
William Galvão <Liam_draven@outlook.com>
2025-02-10 17:51:26 -0300

    Update StringTest01.java

--- src/academy/devdojo/maratonajava/javacore/Aula017Strings/test/StringTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aula017Strings/test/StringTest01.java
@@ -4,7 +4,7 @@ public class StringTest01 {
     public static void main(String[] args) {
         String nome = "William"; // String constant pool
         String nome2 = "William";
-        nome.concat(" Galvão");
+        nome.concat(" Galvão"); // nome += " Galvão"
         // String são imutáveis
         System.out.println(nome == nome2); // Comparando as strings
         String nome3 = new String("William"); // Variável de referencia, Objeto do tipo string
