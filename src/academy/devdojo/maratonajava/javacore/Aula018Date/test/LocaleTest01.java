1847609324dd3bc5b7f07f87ab0a481571408d2d
William Galvão <Liam_draven@outlook.com>
2025-02-11 18:03:39 -0300

    Create LocaleTest01.java

---
+++ src/academy/devdojo/maratonajava/javacore/Aula018Date/test/LocaleTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Aula018Date.test;
+
+import java.util.Locale;
+
+public class LocaleTest01 {
+    public static void main(String[] args) {
+        Locale localeItaly = new Locale("it","IT");
+
+    }
+}
