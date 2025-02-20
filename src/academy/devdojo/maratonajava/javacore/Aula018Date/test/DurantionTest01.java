8d73b94fc3f81ac41e98d997ff0c62d85b770bc3
William Galvão <Liam_draven@outlook.com>
2025-02-20 18:04:52 -0300

    Update DurantionTest01.java

--- src/academy/devdojo/maratonajava/javacore/Aula018Date/test/DurantionTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aula018Date/test/DurantionTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Aula018Date.test;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
@@ -11,6 +12,14 @@ public class DurantionTest01 {
         LocalTime timeNow = LocalTime.now();
         LocalTime timeNinus7Hours = LocalTime.now().minusHours(7);
         Duration d1 = Duration.between(now,nowAfterTwoYears);
+        Duration d2 = Duration.between(timeNow,timeNinus7Hours);
+        Duration d3 = Duration.between(Instant.now(), Instant.now().plusSeconds(1000));
+        Duration d4 = Duration.ofMinutes(3);
+        Duration d5 = Duration.ofDays(20);
         System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+        System.out.println(d4);
+        System.out.println(d5);
     }
 }
