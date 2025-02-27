0f14507e970d596e446f610b7608c5e079176b3c
William Galvão <Liam_draven@outlook.com>
2025-02-26 21:29:04 -0300

    127

---
+++ src/academy/devdojo/maratonajava/javacore/Aula018Date/test/TemporalAdjustersTest02.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Aula018Date.test;
+
+import java.time.LocalDate;
+
+public class TemporalAdjustersTest02 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+    }
+}
