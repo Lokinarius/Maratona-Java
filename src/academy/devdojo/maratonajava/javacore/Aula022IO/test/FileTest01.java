81f41c638f87fa7c8b61a45cf98d235bfa801b66
William Galvão <Liam_draven@outlook.com>
2025-03-09 17:17:52 -0300

    Update FileTest01.java

--- src/academy/devdojo/maratonajava/javacore/Aula022IO/test/FileTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aula022IO/test/FileTest01.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.javacore.Aula022IO.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Instant;
 
 public class FileTest01 {
     public static void main(String[] args) {
@@ -12,6 +13,10 @@ public class FileTest01 {
             System.out.println("Created "+isCreated);
             System.out.println("path "+file.getPath());
             System.out.println("path absolute "+ file.getAbsolutePath());
+            System.out.println("is directory "+file.isDirectory());
+            System.out.println("is file "+file.isFile());
+            System.out.println("is hidden "+file.isHidden());
+            System.out.println("last modified "+ Instant.ofEpochMilli(file.lastModified()));
             boolean exists = file.exists();
             if(exists){
                 System.out.println("Deleted "+file.delete());
